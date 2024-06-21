94db1a04d31e50d12a34e9f348fb456466c1120e
Catastasis <CHNSOC@gmail.com>
2024-06-21 20:31:34 +0700

    Fix broken anchor link

--- src/scenes/skills/SkillCard.tsx
+++ src/scenes/skills/SkillCard.tsx
@@ -1,37 +1,49 @@
-import useMediaQuery from '@/hooks/useMediaQuery'
-import { SelectedPage } from '@/shared/types'
-import { motion } from 'framer-motion'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const childVariant = { hidden: { opacity: 0, scale: 0.9 }, visible: { opacity: 1, scale: 1 } }
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
-type Props = { icon?: JSX.Element, image?: JSX.Element, title: string, description: JSX.Element, setSelectedPage: (value: SelectedPage) => void }
+type Props = {
+  icon?: JSX.Element;
+  image?: JSX.Element;
+  title: string;
+  description: JSX.Element;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
 function SkillCard({ icon, title, description, setSelectedPage }: Props) {
-    const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
-    return (
-        <motion.div
-            
-            className={isAboveMediumScreen? ('flex flex-col px-5 py-16 text-center md:max-w-none w-2/5 h-2/3 border-2 border-gray-400') 
-            : ('flex-col text-center px-5 py-16 border-2 max-w-md mx-auto space-y-5 my-8 first:mt-12' )}
-            variants={childVariant}
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  return (
+    <motion.div
+      className={
+        isAboveMediumScreen
+          ? "flex h-2/3 w-2/5 flex-col border-2 border-gray-400 px-5 py-16 text-center md:max-w-none"
+          : "mx-auto my-8 max-w-md flex-col space-y-5 border-2 px-5 py-16 text-center first:mt-12"
+      }
+      variants={childVariant}
+    >
+      <div className="mb-4 flex justify-center">
+        <div className="bg-primary-100 rounded-full border-2 border-gray-100 p-4">
+          {icon}
+        </div>
+      </div>
+      <h4 className="font-bold">{title}</h4>
+      <div className="mt-12">{description}</div>
+      <div className="mx-12 mt-auto h-auto w-auto border-spacing-2 border-2 border-gray-400 py-4 hover:bg-gray-100">
+        <a
+          className="text-primary-500 hover:text-secondary-500 text-xl font-bold underline"
+          href={"https://csoc-homesite.vercel.app/netrunner"}
         >
-            <div className='mb-4 flex justify-center'>
-                <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
-                    {icon}
-                </div>
-            </div>
-            <h4 className='font-bold'>{title}</h4>
-            <div className='mt-12'>{description}</div>
-            <div className='w-auto h-auto border-spacing-2 border-gray-400 border-2 mt-auto py-4 mx-12 hover:bg-gray-100'>
-                <AnchorLink className="text-xl font-bold text-primary-500 underline hover:text-secondary-500"
-                    onClick={() => { setSelectedPage(SelectedPage.Contact) }}
-                    href={"https://csoc-homesite.vercel.app/netrunner"}>
-                    <button className=''>Explore</button>
-                </AnchorLink>
-            </div>
-        </motion.div>
-    )
+          <button className="">Explore</button>
+        </a>
+      </div>
+    </motion.div>
+  );
 }
 
-export default SkillCard
+export default SkillCard;
